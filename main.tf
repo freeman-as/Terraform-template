0456367d3a3cd35b3b889aa92fa0468649640510
Atsunori Sagawa <freeman.by2018@gmail.com>
2021-09-19 16:06:43 +0900

    add: VPC, Subnet作成

--- main.tf
+++ main.tf
@@ -1,19 +1,50 @@
-provider "aws" {
-  profile = "terraform"
-  region = "ap-northeast-1"
+# ----------------------------------------
+# Terraform configuration
+# ----------------------------------------
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
 }
 
-resource "aws_instance" "hello-world" {
-  ami = "ami-02892a4ea9bfa2192"
-  instance_type = "t2.micro"
+# ----------------------------------------
+# Provider
+# ----------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
 
-  tags = {
-    Name = "HelloWorld"
-  }
+# ----------------------------------------
+# Resource
+# ----------------------------------------
 
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF
+# resource "aws_instance" "hello-world" {
+#   ami           = "ami-02892a4ea9bfa2192"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+
+#   user_data = <<EOF
+# #!/bin/bash
+# amazon-linux-extras install -y nginx1.12
+# systemctl start nginx
+# EOF
+# }
+
+# ----------------------------------------
+# Variables
+# ----------------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
