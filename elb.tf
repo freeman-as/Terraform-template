d361d2f5354300c99ac907af930e224348185900
Atsunori Sagawa <freeman.by2018@gmail.com>
2021-09-20 09:29:31 +0900

    add: resource alb target group

--- elb.tf
+++ elb.tf
@@ -14,3 +14,23 @@ resource "aws_lb" "alb" {
   ]
 }
 
+# ----------------------------------------
+# Target Group
+# ----------------------------------------
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.environment}-app-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-tg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id
+}
