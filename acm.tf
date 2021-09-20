8b505e4940586478d402587f635fc39195d9e660
Atsunori Sagawa <freeman.by2018@gmail.com>
2021-09-20 10:18:40 +0900

    add: resouce acm certificate

---
+++ acm.tf
@@ -0,0 +1,23 @@
+# ----------------------------------------
+# Cergificate
+# ----------------------------------------
+# for tokyo region
+resource "aws_acm_certificate" "tokyo_cert" {
+  domain_name       = "*.${var.domain}"
+  validation_method = "DNS"
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-wildecard-sslcert"
+    Project = var.project
+    Env     = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_route53_zone.route53_zone
+  ]
+}
+
